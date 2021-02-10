cea0b826a95a428224e23f86900e1aab23faa7b6
bboczula <boczula.bartosz@gmail.com>
2021-02-10 21:31:36 +0100

    Removed comments from the message loop.

--- CH01/SapphireEngine/WindowApplication.cpp
+++ CH01/SapphireEngine/WindowApplication.cpp
@@ -39,27 +39,18 @@ void Sapphire::WindowApplication::Run()
 
 	Initialize();
 
-	// For each thread that creates the window, the OS creates a queue for window messages.
-	// This queue holds messages  for all windows that are created on that thread.
 	MSG msg{ 0 };
 	while (1)
 	{
-		//// If HWND parameter is NULL, then function retrieves messages for any window that belongs to current thread,
-		//// so both window messages and thread messages.
 		bool isMessageAvailable = PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
 		if (isMessageAvailable)
 		{
-			// WM_QUIT meanst stop the message loop and exit the application
 			if (msg.message == WM_QUIT)
 			{
 				std::cout << "  Sapphire::WindowApplication::Run() - WM_QUIT received" << std::endl;
 				break;
 			}
 
-			// This function translates keystrokes into characters.
-			// TranslateMessage(&msg);
-
-			// This function tells OS to call Window Procedure
 			DispatchMessage(&msg);
 		}
 		
