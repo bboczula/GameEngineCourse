078c68658f6fae94216f3f0d926550a6452abec6
bboczula <boczula.bartosz@gmail.com>
2021-02-24 08:58:07 +0100

    Got rid of the console completly. Updated gitignore

--- CH02/SapphireReference/SapphireReference.cpp
+++ CH02/SapphireReference/SapphireReference.cpp
@@ -5,7 +5,7 @@
 #include <Windows.h>
 #include "../SapphireEngine/Engine.h"
 
-int main()
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int nCmdShow)
 {
     Sapphire::Engine engine;
     engine.Run();
