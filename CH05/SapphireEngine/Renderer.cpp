575868b4fb95af53d9cc6bc8a1abeee32c6b94b0
bboczula <boczula.bartosz@gmail.com>
2021-06-28 10:55:00 +0200

    Tiny refactoring

--- CH05/SapphireEngine/Renderer.cpp
+++ CH05/SapphireEngine/Renderer.cpp
@@ -249,8 +249,10 @@ void Sapphire::Renderer::CreateSwapChain()
 void Sapphire::Renderer::ReportLiveObjects()
 {
 	IDXGIDebug* dxgiControler;
-	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiControler)))) {
-		dxgiControler->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_FLAGS(DXGI_DEBUG_RLO_SUMMARY | DXGI_DEBUG_RLO_IGNORE_INTERNAL));
+	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiControler))))
+	{
+		dxgiControler->ReportLiveObjects(DXGI_DEBUG_ALL,
+			DXGI_DEBUG_RLO_FLAGS(DXGI_DEBUG_RLO_SUMMARY | DXGI_DEBUG_RLO_IGNORE_INTERNAL));
 	}
 	SafeRelease(&dxgiControler);
 }
