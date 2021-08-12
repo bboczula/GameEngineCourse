f6e9c9828ac1d4d8ab242b0e6d1b6f3943fd58ee
bboczula <boczula.bartosz@gmail.com>
2021-08-12 11:46:30 +0200

    Added experimental XML comments to the CommandQueue class.

--- CH06/SapphireEngine/CommandQueue.h
+++ CH06/SapphireEngine/CommandQueue.h
@@ -14,10 +14,29 @@ namespace Sapphire
 	class CommandQueue
 	{
 	public:
+		/// <summary>
+		/// Creates new CommandQeueue instance.
+		/// </summary>
+		/// <param name="device">DirectX 12 device used to create new Command Queue</param>
 		CommandQueue(ID3D12Device* device);
+
 		~CommandQueue();
+
+		/// <summary>
+		/// Returns a pointer for wrapped DirectX 12 Command Queue interface.
+		/// </summary>
+		/// <returns>A raw pointer to ID3D12CommandQueue</returns>
 		ID3D12CommandQueue* Get();
+
+		/// <summary>
+		/// Executes provided command list and fulshes the GPU queue.
+		/// </summary>
+		/// <param name="commandList">Single command list to execute</param>
 		void Execute(ID3D12CommandList* commandList);
+
+		/// <summary>
+		/// Increments the GPU fence and halts current CPU thread until this value is reached.
+		/// </summary>
 		void Flush();
 	private:
 		ID3D12CommandQueue* commandQueue;
