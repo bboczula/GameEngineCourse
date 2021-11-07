cb9015e8a0a66e1a6a385cdc08506e7707a065b2
bboczula <boczula.bartosz@gmail.com>
2021-11-07 22:29:34 +0100

    Added comments to all the new class headers.

--- CH07/SapphireEngine/DX12DescriptorHeap.h
+++ CH07/SapphireEngine/DX12DescriptorHeap.h
@@ -7,11 +7,24 @@
 
 namespace Sapphire
 {
+	/// <summary>
+	/// A wrapper class for the ID3D12DescriptorHeap interface. It allocates the fixed amount of descriptors and then keeps the index of the first one available.
+	/// </summary>
 	class DX12DescriptorHeap
 	{
 	public:
-		DX12DescriptorHeap(ID3D12Device* device, D3D12_DESCRIPTOR_HEAP_TYPE);
+		/// <summary>
+		/// Creates new Descriptor Heap instance of the given type.
+		/// </summary>
+		/// <param name="device">DirectX 12 device used to create new Descriptor Heap</param>
+		/// <param name="type">A type of the new Descriptor Heap</param>
+		DX12DescriptorHeap(ID3D12Device* device, D3D12_DESCRIPTOR_HEAP_TYPE type);
+
 		~DX12DescriptorHeap();
+
+		/// <summary>
+		/// Returns the first available descriptor handle, then moves the internal index one position up.
+		/// </summary>
 		SIZE_T AllocateDescriptor();
 	private:
 		ID3D12DescriptorHeap* heap;
--- CH07/SapphireEngine/DX12RenderTarget.h
+++ CH07/SapphireEngine/DX12RenderTarget.h
@@ -4,11 +4,22 @@
 
 namespace Sapphire
 {
+	/// <summary>
+	/// This class would be an equivalend of the Render Target View class, if one existed in DirectX 12.
+	/// </summary>
 	class DX12RenderTarget
 	{
 		friend class DX12CommandList;
 	public:
+		/// <summary>
+		/// Creates new Render Target instance and stores pointers to associated resource, descriptor handle and the initial resource state.
+		/// </summary>
+		/// <param name="device">DirectX 12 device used to create new Render Target</param>
+		/// <param name="resource">A resource associated with the Render Target</param>
+		/// <param name="descriptor">A Descriptor Handle to use during Render Target creation</param>
+		/// <param name="state">An initial state of the resource</param>
 		DX12RenderTarget(ID3D12Device* device, ID3D12Resource* resource, D3D12_CPU_DESCRIPTOR_HANDLE descriptor, D3D12_RESOURCE_STATES state);
+
 		~DX12RenderTarget();
 	private:
 		ID3D12Resource* resource;
