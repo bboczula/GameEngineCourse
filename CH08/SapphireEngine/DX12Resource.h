6a734bb6b192e59fdec127490812e04b34b7e9d5
bboczula <boczula.bartosz@gmail.com>
2021-12-19 14:49:32 +0100

    Added code documentation to DX12Resource

--- CH08/SapphireEngine/DX12Resource.h
+++ CH08/SapphireEngine/DX12Resource.h
@@ -14,8 +14,17 @@ namespace Sapphire
 	{
 		friend class DX12CommandList;
 	public:
+		/// <summary>
+		/// Creates DirectX 12 resource, based on existing resource created externally.
+		/// </summary>
+		/// <param name="source">Pointer to a resource created externally</param>
+		/// <param name="state">Initial state of the resource</param>
 		DX12Resource(void* source, D3D12_RESOURCE_STATES state);
+
 		~DX12Resource();
+		/// <summary>
+		/// Returns raw pointer to the resource.
+		/// </summary>
 		ID3D12Resource* GetResource();
 	private:
 		D3D12_RESOURCE_STATES state;
