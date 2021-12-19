1338ade984b9b16fb2cb8c790326ffc1577a6283
bboczula <boczula.bartosz@gmail.com>
2021-12-19 14:46:33 +0100

    Added documentation for DX12Device class

--- CH08/SapphireEngine/DX12Device.h
+++ CH08/SapphireEngine/DX12Device.h
@@ -14,10 +14,22 @@ namespace Sapphire
 	class DX12Device
 	{
 	public:
+		/// <summary>
+		/// Creates instance of the DirectX 12 Device.
+		/// </summary>
+		/// <param name="adapter">DXGI adapter to bound to the new Device</param>
 		DX12Device(IDXGIAdapter1* adapter);
+
 		~DX12Device();
+
+		/// <summary>
+		/// Returns raw pointer to the Device.
+		/// </summary>
 		ID3D12Device* GetDevice();
 	private:
+		/// <summary>
+		/// Enable debug layers, both on the API-level and GPU-level.
+		/// </summary>
 		void EnableDebugLayer();
 		ID3D12Device* device;
 	};
