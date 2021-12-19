ef238cb543b3049f59f9830909613fc28ada5eec
bboczula <boczula.bartosz@gmail.com>
2021-12-19 14:51:39 +0100

    Added documentation to DX12Shader class

--- CH08/SapphireEngine/DX12Shader.h
+++ CH08/SapphireEngine/DX12Shader.h
@@ -14,8 +14,17 @@ namespace Sapphire
 	class DX12Shader
 	{
 	public:
+		/// <summary>
+		/// Creates new shader instance using compiled shader file.
+		/// </summary>
+		/// <param name="source">Name of the file containing compiled shader code.</param>
 		DX12Shader(LPCSTR fileName);
+
 		~DX12Shader();
+
+		/// <summary>
+		/// Returns a copy of the compiled shader code and its size.
+		/// </summary>
 		D3D12_SHADER_BYTECODE GetBytecode();
 	private:
 		D3D12_SHADER_BYTECODE bytecode;
