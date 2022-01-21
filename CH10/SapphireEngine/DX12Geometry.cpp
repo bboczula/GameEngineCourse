e81866e33d3228facb9e73995d36bc8df6672ef0
bboczula <boczula.bartosz@gmail.com>
2022-01-21 07:33:01 +0100

    One more parameter name change

--- CH10/SapphireEngine/DX12Geometry.cpp
+++ CH10/SapphireEngine/DX12Geometry.cpp
@@ -1,12 +1,12 @@
 #include "DX12Geometry.h"
 
-Sapphire::DX12Geometry::DX12Geometry(DX12Device* device, void* triangleVertices, UINT vertexSize, UINT numOfVertices) : numOfVertices(numOfVertices)
+Sapphire::DX12Geometry::DX12Geometry(DX12Device* device, void* data, UINT vertexSize, UINT numOfVertices) : numOfVertices(numOfVertices)
 {
 	Logger::GetInstance().Log("%s\n", "Sapphire::DX12Geometry::DX12Geometry()");
 
 	UINT bufferSize = vertexSize * numOfVertices;
 	vertexBuffer = new DX12Resource(device, bufferSize);
-	vertexBuffer->Upload(triangleVertices, bufferSize);
+	vertexBuffer->Upload(data, bufferSize);
 
 	vertexBufferView.BufferLocation = vertexBuffer->GetGpuVirtualAddress();
 	vertexBufferView.StrideInBytes = vertexSize;
